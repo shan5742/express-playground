cee5c40988da52467b40f6f39d3e3680103ebf84
shan5742 <asamshan456@gmail.com>
2020-09-02 14:06:43 +0100

    finalising basic styles for weather app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
 console.log("client side JS loaded");
+
+const weatherForm = document.querySelector("form");
+const searchTerm = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = searchTerm.value;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:3000/weather?location=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return (messageOne.textContent = data.error);
+        }
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      });
+    }
+  );
+});
