081e3a07ff1e59cef69a2680d34f96747793b394
shan5742 <asamshan456@gmail.com>
2020-09-02 08:48:33 +0100

    adding more styles for pages, creating favicon and some other basic frontend stuff

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 // Define paths for Express
 const publicDirPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //Setup handlebars template
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather App",
-    heading: "Welcome to the weather App",
+    heading: "Weather App",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "About Page",
     heading: "About",
     caption: "This is me",
   });
@@ -29,7 +30,6 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help Page",
     heading: "Help",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, praesentium quas quidem asperiores numquam enim ex et. In, tenetur eaque id, eligendi beatae esse corporis sit, quae molestiae non ad?",
@@ -40,6 +40,20 @@ app.get("/weather", (req, res) => {
   res.send({
     temperature: 15,
     feelslike: 14,
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    heading: "404",
+    message: "Oops, help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    heading: "404",
+    message: "Oops, something went wrong",
   });
 });
 
