dd8c59de2c0e803c1b8b82d1cfbaa320eee98080
shan5742 <asamshan456@gmail.com>
2020-09-02 08:01:12 +0100

    switching to handlebars and organising app.js more with some comments

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # express-playground
+
+Setting up of express server, experimenting with serving up static html files and assets. I have since switched to using handlebars for creating dynamic templates.
+
+Working with various express methods such as `app.get()`, `app.set()`, `app.use()` and more.
--- src/app.js
+++ src/app.js
@@ -2,8 +2,39 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+// Define paths for Express
 const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
+
+//Setup handlebars template
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
 app.use(express.static(publicDirPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    heading: "Welcome to the weather App",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Page",
+    heading: "About",
+    caption: "This is me",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    heading: "Help",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, praesentium quas quidem asperiores numquam enim ex et. In, tenetur eaque id, eligendi beatae esse corporis sit, quae molestiae non ad?",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -12,6 +43,7 @@ app.get("/weather", (req, res) => {
   });
 });
 
+// Server running info
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });
